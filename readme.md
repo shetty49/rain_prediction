929a0389a7a9e760fcdf3848c4a578fcf3a82b8c
pranamshetty <pranamshetty49@gmail.com>
2024-09-27 18:33:48 +0530

    Initial commit for rain prediction project

---
+++ readme.md
@@ -0,0 +1,5 @@
+# Rain Prediction
+
+This project is designed for Rain Prediction using machine learning.
+
+## Project Structure
---
+++ src/welcome_to_colab.py
@@ -0,0 +1,254 @@
+import numpy as np
+import sys
+import pandas as pd
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+import datetime
+from sklearn.preprocessing import LabelEncoder
+from sklearn import preprocessing
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+import seaborn as sns
+from keras.layers import Dense, BatchNormalization, Dropout, LSTM
+from keras.models import Sequential
+from keras.utils import to_categorical
+from keras.optimizers import Adam
+from tensorflow.keras import regularizers
+from sklearn.metrics import precision_score, recall_score, confusion_matrix, classification_report, accuracy_score, f1_score
+from keras import callbacks
+
+np.random.seed(0)
+
+if('google.colab' in sys.modules):
+    from google.colab import drive
+    drive.mount('/content/drive', force_remount = True)
+    DIR = '/content/drive/MyDrive/kaagle'
+    file_loc = DIR + "/weatherAUS.csv"
+
+data = pd.read_csv(file_loc)
+data.head()
+
+data.info()
+
+#first of all let us evaluate the target and find out if our data is imbalanced or not
+cols= ["#C2C4E2","#EED4E5"]
+sns.countplot(x= data["RainTomorrow"], palette= cols)
+
+lengths = data["Date"].str.len()
+lengths.value_counts()
+
+#There don't seem to be any error in dates so parsing values into datetime
+data['Date']= pd.to_datetime(data["Date"])
+#Creating a collumn of year
+data['year'] = data.Date.dt.year
+
+# function to encode datetime into cyclic parameters.
+#As I am planning to use this data in a neural network I prefer the months and days in a cyclic continuous feature.
+
+def encode(data, col, max_val):
+    data[col + '_sin'] = np.sin(2 * np.pi * data[col]/max_val)
+    data[col + '_cos'] = np.cos(2 * np.pi * data[col]/max_val)
+    return data
+
+data['month'] = data.Date.dt.month
+data = encode(data, 'month', 12)
+
+data['day'] = data.Date.dt.day
+data = encode(data, 'day', 31)
+
+data.head()
+
+# roughly a year's span section
+section = data[:360]
+tm = section["day"].plot(color="#C2C4E2")
+tm.set_title("Distribution Of Days Over Year")
+tm.set_ylabel("Days In month")
+tm.set_xlabel("Days In Year")
+
+cyclic_month = sns.scatterplot(x="month_sin",y="month_cos",data=data, color="#C2C4E2")
+cyclic_month.set_title("Cyclic Encoding of Month")
+cyclic_month.set_ylabel("Cosine Encoded Months")
+cyclic_month.set_xlabel("Sine Encoded Months")
+
+cyclic_day = sns.scatterplot(x='day_sin',y='day_cos',data=data, color="#C2C4E2")
+cyclic_day.set_title("Cyclic Encoding of Day")
+cyclic_day.set_ylabel("Cosine Encoded Day")
+cyclic_day.set_xlabel("Sine Encoded Day")
+
+# Get list of categorical variables
+s = (data.dtypes == "object")
+object_cols = list(s[s].index)
+
+print("Categorical variables:")
+print(object_cols)
+
+# Missing values in categorical variables
+
+for i in object_cols:
+    print(i, data[i].isnull().sum())
+
+# Filling missing values with mode of the column in value
+
+for i in object_cols:
+    data[i].fillna(data[i].mode()[0], inplace=True)
+
+# Get list of neumeric variables
+t = (data.dtypes == "float64")
+num_cols = list(t[t].index)
+
+print("Neumeric variables:")
+print(num_cols)
+
+# Missing values in numeric variables
+
+for i in num_cols:
+    print(i, data[i].isnull().sum())
+
+# Filling missing values with median of the column in value
+
+for i in num_cols:
+    data[i].fillna(data[i].median(), inplace=True)
+
+data.info()
+
+# Apply label encoder to each column with categorical data
+label_encoder = LabelEncoder()
+for i in object_cols:
+    data[i] = label_encoder.fit_transform(data[i])
+
+data.info()
+
+# Prepairing attributes of scale data
+
+features = data.drop(['RainTomorrow', 'Date','day', 'month'], axis=1) # dropping target and extra columns
+
+target = data['RainTomorrow']
+
+#Set up a standard scaler for the features
+col_names = list(features.columns)
+s_scaler = preprocessing.StandardScaler()
+features = s_scaler.fit_transform(features)
+features = pd.DataFrame(features, columns=col_names)
+
+features.describe().T
+
+#Detecting outliers
+#looking at the scaled features
+colours = ["#D0DBEE", "#C2C4E2", "#EED4E5", "#D1E6DC", "#BDE2E2"]
+plt.figure(figsize=(20,10))
+sns.boxenplot(data = features,palette = colours)
+plt.xticks(rotation=90)
+plt.show()
+
+#full data for
+features["RainTomorrow"] = target
+
+#Dropping with outlier
+
+features = features[(features["MinTemp"]<2.3)&(features["MinTemp"]>-2.3)]
+features = features[(features["MaxTemp"]<2.3)&(features["MaxTemp"]>-2)]
+features = features[(features["Rainfall"]<4.5)]
+features = features[(features["Evaporation"]<2.8)]
+features = features[(features["Sunshine"]<2.1)]
+features = features[(features["WindGustSpeed"]<4)&(features["WindGustSpeed"]>-4)]
+features = features[(features["WindSpeed9am"]<4)]
+features = features[(features["WindSpeed3pm"]<2.5)]
+features = features[(features["Humidity9am"]>-3)]
+features = features[(features["Humidity3pm"]>-2.2)]
+features = features[(features["Pressure9am"]< 2)&(features["Pressure9am"]>-2.7)]
+features = features[(features["Pressure3pm"]< 2)&(features["Pressure3pm"]>-2.7)]
+features = features[(features["Cloud9am"]<1.8)]
+features = features[(features["Cloud3pm"]<2)]
+features = features[(features["Temp9am"]<2.3)&(features["Temp9am"]>-2)]
+features = features[(features["Temp3pm"]<2.3)&(features["Temp3pm"]>-2)]
+
+
+features.shape
+
+#looking at the scaled features without outliers
+
+plt.figure(figsize=(20,10))
+sns.boxenplot(data = features,palette = colours)
+plt.xticks(rotation=90)
+plt.show()
+
+X = features.drop(["RainTomorrow"], axis=1)
+y = features["RainTomorrow"]
+
+# Splitting test and training sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 42)
+
+X.shape
+
+#Early stopping
+early_stopping = callbacks.EarlyStopping(
+    min_delta=0.001, # minimium amount of change to count as an improvement
+    patience=8, # how many epochs to wait before stopping
+    restore_best_weights=True,
+)
+
+# Initialising the NN
+model = Sequential()
+
+# layers
+
+model.add(Dense(units = 32, kernel_initializer = 'uniform', activation = 'relu', input_dim = 26))
+model.add(Dense(units = 32, kernel_initializer = 'uniform', activation = 'relu'))
+model.add(Dense(units = 16, kernel_initializer = 'uniform', activation = 'relu'))
+model.add(Dropout(0.25))
+model.add(Dense(units = 8, kernel_initializer = 'uniform', activation = 'relu'))
+model.add(Dropout(0.5))
+model.add(Dense(units = 1, kernel_initializer = 'uniform', activation = 'sigmoid'))
+
+# Compiling the ANN
+opt = Adam(learning_rate=0.00009)
+model.compile(optimizer = opt, loss = 'binary_crossentropy', metrics = ['accuracy'])
+
+# Train the ANN
+history = model.fit(X_train, y_train, batch_size = 32, epochs = 150, callbacks=[early_stopping], validation_split=0.2)
+
+history_df = pd.DataFrame(history.history)
+
+plt.plot(history_df.loc[:, ['loss']], "#BDE2E2", label='Training loss')
+plt.plot(history_df.loc[:, ['val_loss']],"#C2C4E2", label='Validation loss')
+plt.title('Training and Validation loss')
+plt.xlabel('Epochs')
+plt.ylabel('Loss')
+plt.legend(loc="best")
+
+plt.show()
+
+history_df = pd.DataFrame(history.history)
+
+plt.plot(history_df.loc[:, ['accuracy']], "#BDE2E2", label='Training accuracy')
+plt.plot(history_df.loc[:, ['val_accuracy']], "#C2C4E2", label='Validation accuracy')
+
+plt.title('Training and Validation accuracy')
+plt.xlabel('Epochs')
+plt.ylabel('Accuracy')
+plt.legend()
+plt.show()
+
+# Predicting the test set results
+y_pred = model.predict(X_test)
+y_pred = (y_pred > 0.5)
+
+# confusion matrix
+cmap1 = sns.diverging_palette(260,-10,s=50, l=75, n=5, as_cmap=True)
+plt.subplots(figsize=(12,8))
+cf_matrix = confusion_matrix(y_test, y_pred)
+sns.heatmap(cf_matrix/np.sum(cf_matrix), cmap = cmap1, annot = True, annot_kws = {'size':15})
+
+print(classification_report(y_test, y_pred))
+
+
+
+
+
+
+
+
+
+
+
