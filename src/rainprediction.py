5b02e9d1a804ff229a130d08c2c3cb76583c6314
shetty49 <95335320+shetty49@users.noreply.github.com>
2025-01-24 15:09:07 +0530

    Update rainprediction.py

--- src/rainprediction.py
+++ src/rainprediction.py
@@ -294,7 +294,7 @@ html_content = f"""
 # mlflow
 
 # Set up MLflow tracking URI and experiment name
-mlflow.set_tracking_uri("http://127.0.0.1:5000")
+mlflow.set_tracking_uri("http://127.0.0.1:5001")
 mlflow.set_experiment("rain_prediction")
 
 # Start an MLflow run
